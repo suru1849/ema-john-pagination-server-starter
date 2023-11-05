e0a267df7cad6a3638d4987ac1a5773bbce941f3
suru1849 <suranjan1849@gmail.com>
2023-11-05 18:30:21 +0600

    Final push

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 console.log(process.env.DB_USER, process.env.DB_PASS);
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kapryhp.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -29,7 +29,28 @@ async function run() {
     const productCollection = client.db("emaJohnDB").collection("products");
 
     app.get("/products", async (req, res) => {
-      const result = await productCollection.find().toArray();
+      const page = parseInt(req.query.page);
+      const size = parseInt(req.query.size);
+      console.log("Pegination", page, size);
+
+      const result = await productCollection
+        .find()
+        .skip(page * size)
+        .limit(size)
+        .toArray();
+      res.send(result);
+    });
+
+    app.post("/productsByIds", async (req, res) => {
+      const ids = req.body;
+      const idsWithObjectIds = ids.map((id) => new ObjectId(id));
+      console.log(idsWithObjectIds);
+      const query = {
+        _id: {
+          $in: idsWithObjectIds,
+        },
+      };
+      const result = await productCollection.find(query).toArray();
       res.send(result);
     });
 
